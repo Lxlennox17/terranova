c9cdf836a472c72e09f20305d3fcb61e87ecd836
Lxlennox17 <lennox.wunsch@icloud.com>
2024-12-29 14:27:02 +0100

    Terranova

---
+++ src/main/java/net/lxlennox/terranova/TerranovaClient.java
@@ -0,0 +1,10 @@
+package net.lxlennox.terranova;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TerranovaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
